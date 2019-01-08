46890d982bc85d4e896b1a1cba3929f301056180
Neeraj Singh <NeSingh@ad.prolifics.com>
2019-01-08 14:17:36 +0530

    Updated script

--- utilities/base_ui.py
+++ utilities/base_ui.py
@@ -25,7 +25,7 @@ class BaseUI():
 
 	def launch_browser(self):
 		options = webdriver.ChromeOptions()
-		chrome_options.add_argument('--headless')
+		options.add_argument('--headless')
 		options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 		driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', chrome_options=options)
 
